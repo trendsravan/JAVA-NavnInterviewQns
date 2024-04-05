3cb9f75bcd99d663c2d0ec0b4e988c05f5007514
srava <srava@sravankumar>
2024-04-05 20:20:12 +0530

    "Second Commit"

--- NaveenInterviewQns/src/Questions01to10/VD05DuplicateElemnts5.java
+++ NaveenInterviewQns/src/Questions01to10/VD05DuplicateElemnts5.java
@@ -4,22 +4,32 @@ import java.util.HashSet;
 
 public class VD05DuplicateElemnts5 {
 	public static void main(String[] args) {
-		 String[] name = {"Java", "JavaScript","Ruby","c","c","Java"};
+		 String[] name = {"Java", "JavaScript","Ruby","c","C","JaVa"};
 		 
 		 
 		 	//By Using For Loop[0(n)]
 		 for (int i =0; i<=name.length-1; i++) {
 			 for(int j=i+1; j<=name.length-1; j++) {
-				if( name[i].equals(name[j])) {
-					System.out.println(name[i]);}}}
+				if( name[i].equalsIgnoreCase(name[j])) {
+					System.out.println("Logic By For Loop =" + name[i]);}}}
 		 
 		 	//By Using HashSet
-		 Set<String> obj =new HashSet();
+		 Set<String> obj =new HashSet<String>();
 		 for(String names:name) {
-			 if(obj.add(names)==false) {
-				 System.out.println(names);}}
+			String LC = names.toLowerCase();
+			 if(obj.add(LC)==false) {
+				 System.out.println("Logic By HashSet =" +names);}}
 			 
-		 
+		 //By Using HashSET & ignoreCase
+		 Set<String> uniqueElements = new HashSet<>();
+	        Set<String> duplicates = new HashSet<>();
+	        for (String element : name) {
+	            String lowercaseElement = element.toLowerCase(); // Convert to lowercase
+	            if (!uniqueElements.add(lowercaseElement)) {
+	                duplicates.add(element);
+	            }
+	        }
+	        System.out.println("Duplicates in the array: " + duplicates);
 		
 		 
 		 
--- NaveenInterviewQns/src/Questions11to20/VD19FactorialUsingRec_NonRec.java
+++ NaveenInterviewQns/src/Questions11to20/VD19FactorialUsingRec_NonRec.java
@@ -1,5 +1,11 @@
 package Questions11to20;
 
 public class VD19FactorialUsingRec_NonRec {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
 
 }
